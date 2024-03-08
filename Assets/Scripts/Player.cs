be2cd6a4149bcfc6384fc554a1514eab1d798978
ata kaleli <atakaleli123@gmail.com>
2024-03-08 17:40:27 +0200

    PlayerMoveJumpRotate AnimAdded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,32 +3,43 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-
+    
     private Rigidbody2D rb;
+    private Animator anim;
+
+    [Header("PlayerMove")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
     private float horizontalInput;
 
+    [Header("CollisionChecks")]
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private float groundCheckDistance;
     private bool isGrounded;
     private bool canDoubleJump=true;
+    private bool isFacingRight = true;
+    private int facingDirection = 1;
+    
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
     void Update()
     {
+        AnimationControllers();
+
         CollisionChecks();
 
 
         Move();
         InputChecks();
+        FlipController();
 
         if (isGrounded)
             canDoubleJump = true;
@@ -36,9 +47,21 @@ public class Player : MonoBehaviour
 
     }
 
+    private void AnimationControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
+
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
     private void InputChecks()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             JumpButton();
@@ -61,6 +84,22 @@ public class Player : MonoBehaviour
         
         rb.velocity = new Vector2(moveSpeed * horizontalInput, rb.velocity.y);
     }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (isFacingRight && rb.velocity.x < 0)
+            Flip();
+        else if (!isFacingRight && rb.velocity.x > 0)
+            Flip();
+    }
+
 
     private void CollisionChecks()
     {
