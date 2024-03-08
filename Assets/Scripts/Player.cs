3c1d5b6c32c3c46c26cf85ebc5ede693018ae51d
ata kaleli <atakaleli123@gmail.com>
2024-03-08 12:23:36 +0200

    MoveAndJumpAdded

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private float horizontalInput;
+
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckDistance;
+    private bool isGrounded;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        CollisionChecks();
+
         
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+                Jump();
+        }
+
+
     }
+
+    private void Move()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveSpeed * horizontalInput, rb.velocity.y);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
 }
