a76875eeb089b04d4094c13093d1be9d64014a0e
ata kaleli <atakaleli123@gmail.com>
2024-03-18 11:43:13 +0200

    Enemy_Bluebird created

---
+++ Assets/Scripts/Enemy_BlueBird.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_BlueBird : Enemy
+{
+    
+
+
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+
+
+}
