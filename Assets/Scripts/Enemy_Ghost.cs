2992e24e7cf1cb944c7d50afee7a5681a9bbf249
ata kaleli <atakaleli123@gmail.com>
2024-03-18 19:35:41 +0200

    Ghost dis/appear added

--- Assets/Scripts/Enemy_Ghost.cs
+++ Assets/Scripts/Enemy_Ghost.cs
@@ -7,16 +7,48 @@ public class Enemy_Ghost : Enemy
 
     [Header("Ghost Info")]
     [SerializeField] private float activeTime;
-                     private float activeTimeCounter;
+                     private float activeTimeCounter = 4f;
 
-    
+    private Transform player;
 
     protected override void Start()
     {
         base.Start();
+        player = GameObject.Find("Player").transform;
+        
     }
 
 
+
+    private void Update()
+    {
+        activeTimeCounter -= Time.deltaTime;
+        idleCounter -= Time.deltaTime;
+
+
+        if(activeTimeCounter > 0)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+
+        if(activeTimeCounter<0 && idleCounter < 0 && isAggressive)
+        {
+            anim.SetTrigger("disappear");
+            isAggressive = false;
+            idleCounter = idleTime;
+        }
+        if (activeTimeCounter < 0 && idleCounter < 0 && !isAggressive)
+        {
+            anim.SetTrigger("appear");
+            isAggressive = true;
+            activeTimeCounter = activeTime;
+            
+        }
+
+
+
+
+    }
 
     public override void Damage()
     {
