0444145da9cb5db73848b7b00daa0d8c8b9eb2ad
ata kaleli <atakaleli123@gmail.com>
2024-03-20 22:47:54 +0200

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : Danger
+public class Bullet : Danger // bullet of enemies that can shoot to player
 {
 
     private Rigidbody2D rb;
@@ -17,19 +17,19 @@ public class Bullet : Danger
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(xSpeed, ySpeed);
+        rb.velocity = new Vector2(xSpeed, ySpeed); // based on the speed to the x and y, set the velocity to bullet
 
     }
 
     
 
-    public void SetupSpeed(float x, float y)
+    public void SetupSpeed(float x, float y) // setup the speed of the bullet from different scripts
     {
         xSpeed = x;
         ySpeed = y;
     }
 
-    protected override void OnTriggerEnter2D(Collider2D collision)
+    protected override void OnTriggerEnter2D(Collider2D collision) // if bullet hit anything, then destroy it so it wont go to infinity.
     {
         base.OnTriggerEnter2D(collision);
         Destroy(gameObject);
