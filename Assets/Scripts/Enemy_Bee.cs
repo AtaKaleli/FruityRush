f3bf18f0b71f2178bbed8b70fd93b2ba293f596b
ata kaleli <atakaleli123@gmail.com>
2024-03-20 20:07:36 +0200

    Enemy_Bee comments Added

--- Assets/Scripts/Enemy_Bee.cs
+++ Assets/Scripts/Enemy_Bee.cs
@@ -31,25 +31,25 @@ public class Enemy_Bee : Enemy
     // Update is called once per frame
     void Update()
     {
-        idleCounter -= Time.deltaTime;
+        idleCounter -= Time.deltaTime; //if the bee is on the idle counter, it will not do anything, just stays in the idle.
         if (idleCounter > 0)
             return;
 
 
-        isPlayerDetected = Physics2D.OverlapCircle(playerCheck.position, checkRadius, whoIsPlayer);
+        isPlayerDetected = Physics2D.OverlapCircle(playerCheck.position, checkRadius, whoIsPlayer); // the area where player is detected
 
-        if (isPlayerDetected && !isAggressive)
+        if (isPlayerDetected && !isAggressive) // if player is detected and the bee is not aggressive, set isaggressive to true, and make it faster
         {
             isAggressive = true;
             speed *= 1.5f;
 
         }
 
-        if (!isAggressive)
+        if (!isAggressive) // if the bee is not aggressive and player is not detected, then move between idle points
         {
-            transform.position = Vector2.MoveTowards(transform.position, idlePoints[idlePointIdx].position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, idlePoints[idlePointIdx].position, speed * Time.deltaTime);//each time, move to new idle point
 
-            if (Vector2.Distance(transform.position, idlePoints[idlePointIdx].position) < .15f)
+            if (Vector2.Distance(transform.position, idlePoints[idlePointIdx].position) < .15f) // make sure that bee moves idle points that is closer to him
             {
                 idlePointIdx++;
                 if (idlePointIdx >= idlePoints.Length)
@@ -62,7 +62,7 @@ public class Enemy_Bee : Enemy
 
             Vector2 newPosition = new Vector2(player.transform.position.x, player.transform.position.y + yOffset);
 
-            float xDiff = transform.position.x - player.position.x;
+            float xDiff = transform.position.x - player.position.x; // when the bee and player x is less than .15f in abs, then bee shoots
 
             transform.position = Vector2.MoveTowards(transform.position, newPosition, speed * Time.deltaTime);
 
@@ -76,7 +76,7 @@ public class Enemy_Bee : Enemy
         }
     }
 
-    private void AttackEvent()
+    private void AttackEvent() // attack event of bee, we created new bullet with origin, then shoot, after shoot, make idlecounter set to idle time and aggro to false
     {
         GameObject newBullet = Instantiate(bulletPref, bulletOrigin.transform.position, Quaternion.identity);
         newBullet.GetComponent<Bullet>().SetupSpeed(0, -bulletSpeed);
