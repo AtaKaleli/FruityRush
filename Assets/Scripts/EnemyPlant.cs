b45326c79e76ebc3f9f6414e3c80f94c94086312
ata kaleli <atakaleli123@gmail.com>
2024-03-20 23:29:08 +0200

    Update EnemyPlant.cs

--- Assets/Scripts/EnemyPlant.cs
+++ Assets/Scripts/EnemyPlant.cs
@@ -30,10 +30,10 @@ public class EnemyPlant : Enemy
         if (!playerDetected)
             return;
 
-        bool playerDetection = playerDetected.collider.GetComponent<Player>() != null;
+        bool playerDetection = playerDetected.collider.GetComponent<Player>() != null; // set true or false based on the player detection
        
 
-        if (idleCounter<0 && playerDetection)
+        if (idleCounter<0 && playerDetection) // if idlecounter is less than 0 and playerdetected, then attack to player and set the idleCounter to idleTime
         {
             idleCounter = idleTime;
             anim.SetTrigger("attack");
@@ -41,7 +41,7 @@ public class EnemyPlant : Enemy
     }
 
 
-    private void AttackEvent()
+    private void AttackEvent() // this event is called in the attack animation of plant
     {
         GameObject newBullet = Instantiate(bulletPref, bulletOrigin.transform.position, Quaternion.identity);
         newBullet.GetComponent<Bullet>().SetupSpeed(bulletSpeed * facingDirection, 0);
