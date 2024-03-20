a61806bb1753301553b7bd81c8feb108755945d7
ata kaleli <atakaleli123@gmail.com>
2024-03-20 23:32:09 +0200

    Update Enemy_Rhino.cs

--- Assets/Scripts/Enemy_Rhino.cs
+++ Assets/Scripts/Enemy_Rhino.cs
@@ -22,7 +22,7 @@ public class Enemy_Rhino : Enemy
     protected override void Start()
     {
         base.Start();
-        invincible = true;
+        invincible = true; // set rhino invincle at the beginning
     }
 
     
@@ -33,25 +33,26 @@ public class Enemy_Rhino : Enemy
     {
 
         
-        if (playerDetected)
+        if (playerDetected) // if rhino detects player, then make him aggressive
             isAggressive = true;
 
-        if (!isAggressive)
+        if (!isAggressive) // if he is not aggressive, then walk him around
         {
             WalkAround();
         }
-        else
+        else // if aggressive, then change the velocity of him , make him faster
         {
             rb.velocity = new Vector2(aggroSpeed*facingDirection,rb.velocity.y);
 
-            if (wallDetected && invincible)
+            if (wallDetected && invincible) // if walldetected (rhino hits wall) and invincible, then make him shock, and remove invincible
             {
                 invincible = false;
                 shockTimeCounter = shockTime;
             }
 
-            if(shockTimeCounter <= 0 && !invincible)
-            {
+            if(shockTimeCounter <= 0 && !invincible) // if shock timer runs out to negative and is not invincible
+            { 
+                // set the invincible back again and Flip him , then set its aggro to false
                 invincible = true;
                 Flip();
                 isAggressive = false;
