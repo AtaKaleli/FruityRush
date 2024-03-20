e57ce627b8cf380386f7d90c47c8bf460b2f3596
ata kaleli <atakaleli123@gmail.com>
2024-03-20 22:07:36 +0200

    Update Danger.cs

--- Assets/Scripts/Danger.cs
+++ Assets/Scripts/Danger.cs
@@ -8,7 +8,7 @@ public class Danger : MonoBehaviour
     // this one responsible for giving damage to player
     protected virtual void OnTriggerEnter2D(Collider2D collision) // need to use virtual if we want to use inheritence and override the functions.
     {
-        if(collision.tag == "Player")
+        if(collision.tag == "Player") //if the collision tag is player, then create a temp player and call the knockback function to knock player
         {
             
             Player player = collision.GetComponent<Player>();
