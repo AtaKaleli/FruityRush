ae20abc53d8824bc6fcf0f2a0de0513bd94b3d48
ata kaleli <atakaleli123@gmail.com>
2024-03-21 11:30:43 +0200

    Trunk Move and gotHit added

--- Assets/Scripts/Enemy_Trunk.cs
+++ Assets/Scripts/Enemy_Trunk.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Trunk : MonoBehaviour
+public class Enemy_Trunk : Enemy
 {
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
+        base.Start();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        CollisionChecks();
+        WalkAround();
+        AnimationControllers();
+
+    }
+
+    private void AnimationControllers()
+    {
+        anim.SetFloat("xVelocity", rb.velocity.x);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
     }
 }
