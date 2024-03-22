1e6f1cc397c5855217eb6e7f705bdf4d7029e708
ata kaleli <atakaleli123@gmail.com>
2024-03-22 19:56:04 +0200

    LevelButtonsAdded

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject buttonPref;
+    [SerializeField] private Transform buttonParent;
+
+    private void Start()
     {
-        
+        for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++) //for each level we have, create a button in the main menu
+        {
+            string sceneName = "Level " + i; //create name of the scene
+            
+            GameObject newLevelButton = Instantiate(buttonPref, buttonParent); //instantiate new button
+            newLevelButton.AddComponent<Button>().onClick.AddListener(() => LevelLoader(sceneName)); // then add onclick->listener to it, and pass func.
+            newLevelButton.GetComponentInChildren<TextMeshProUGUI>().text = sceneName;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelLoader(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
+
 }
