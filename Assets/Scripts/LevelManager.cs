3efb8d0752768793563142c173c880fd58926a1c
ata kaleli <atakaleli123@gmail.com>
2024-03-23 14:27:01 +0200

    LevelSelection updated

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -10,14 +10,21 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject buttonPref;
     [SerializeField] private Transform buttonParent;
 
+    [SerializeField] private bool[] levelOpen;
+
     private void Start()
     {
         for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++) //for each level we have, create a button in the main menu
         {
+
+
+            if (!levelOpen[i])
+                return;
+
             string sceneName = "Level " + i; //create name of the scene
             
             GameObject newLevelButton = Instantiate(buttonPref, buttonParent); //instantiate new button
-            newLevelButton.AddComponent<Button>().onClick.AddListener(() => LevelLoader(sceneName)); // then add onclick->listener to it, and pass func.
+            newLevelButton.GetComponent<Button>().onClick.AddListener(() => LevelLoader(sceneName)); // then add onclick->listener to it, and pass func.
             newLevelButton.GetComponentInChildren<TextMeshProUGUI>().text = sceneName;
 
         }
